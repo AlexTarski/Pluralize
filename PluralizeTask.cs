f6008f7178127aef27b6ed1910ab796d295c910f
AlexTarski <145293460+AlexTarski@users.noreply.github.com>
2024-01-08 23:44:24 +0300

    Creating repo

---
+++ PluralizeTask.cs
@@ -0,0 +1,25 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		if ((count == 1 || count % 10 == 1) && count % 100 != 11)
+		{
+			return "рубль";
+        }
+		else if ((count > 1 && count <= 4) || (count % 10 > 1 && count % 10 <= 4))
+		{
+			if (count % 100 != 12 && count % 100 != 13 && count % 100 != 14)
+			{
+                return "рубля";
+            }
+
+            return "рублей";
+        }
+		else
+		{
+			return "рублей";
+		}
+	}
+}
